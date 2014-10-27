855f213839fcd042ac00ac9978b908d94ba6060e
tg5156 <tg5156@hotmail.com>
2014-10-28 03:57:14 +0800

    menu3.c menu4.c badterm.c password.c sizeterm.c

---
+++ badterm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <term.h>
+#include <ncurses.h>
+
+int main()
+{
+    setupterm("unlisted", fileno(stdout), (int *)0);
+    printf("Done.\n");
+    exit(0);
+}
+
